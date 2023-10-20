2b282fff386cc52541eebfbc8ced1ef5467887bf
Würschinger <vi50jepi@fauad.fau.de>
2023-10-20 22:36:16 +0200

    Bestellübersicht und Kundentakttimer

--- pages/display_bestellungen_results.py
+++ pages/display_bestellungen_results.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-import pandas as pd
+import time
 
 # Dateiname der Datenbank
 database_filename = "bestellungen_database.json"
@@ -17,12 +17,30 @@ def display_results():
 
     # Wenn Daten vorhanden sind, diese in einer Tabelle anzeigen
     if bestellungen_data:
-        st.write("Bestellungen:")
-        df = pd.DataFrame(bestellungen_data)
-        st.dataframe(df)
+        for idx, entry in enumerate(bestellungen_data):
+            st.write(f"**Bestellung {idx + 1}**")
+            with st.beta_container():
+                st.write("Bestelldatum und Uhrzeit:", entry["Bestelldatum und Uhrzeit"])
+                st.write("Kunde:", entry["Kunde"])
+                st.write("Sonderwunsch:", entry["Sonderwunsch"])
+                st.write("Variante nach Bestellung:")
+                for kategorie, farbe in entry["Variante nach Bestellung"].items():
+                    st.write(f"{kategorie}: {farbe}")
+            st.write("")  # Neue Zeile für die nächste Bestellung
+
+        # Countdown-Timer
+        timer_placeholder = st.empty()
+        for i in range(10, -1, -1):
+            timer_text = f"<strong><span style='font-size: 2em;'>Timer: {i} Sekunden</span></strong>"
+            timer_placeholder.markdown(timer_text, unsafe_allow_html=True)
+            time.sleep(1)
+
+        # Timer abgeschlossen
+        timer_placeholder.empty()
+        st.write("Countdown-Timer abgelaufen.")
+        st.experimental_rerun()  # Seite neu laden
     else:
         st.write("Keine Bestellungen vorhanden.")
+
 if __name__ == '__main__':
     display_results()
-if st.button("Bestellungen updaten"):
-   st.experimental_rerun()
