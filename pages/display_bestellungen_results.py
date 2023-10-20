2f6719589adc36fadcfb4c2877445aba5b4d9dcf
Würschinger <vi50jepi@fauad.fau.de>
2023-10-20 21:33:46 +0200

    Create display_bestellungen_results.py

---
+++ pages/display_bestellungen_results.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import json
+import pandas as pd
+
+# Dateiname der Datenbank
+database_filename = "bestellungen_database.json"
+
+def display_werkzeugnis_results():
+    st.title("Bestellungen")
+
+    # Laden der Werkzeugnisdaten aus der JSON-Datei
+    bestellungen_data = []
+    with open(database_filename, "r") as db:
+        for line in db:
+            bestellungen_info = json.loads(line)
+            werkzeugnis_data.append(bestellungen_info)
+
+    # Wenn Daten vorhanden sind, diese in einer Tabelle anzeigen
+    if bestellungen_data:
+        st.write("Bestellungen:")
+        df = pd.DataFrame(bestellungen_data)
+        st.dataframe(df)
+    else:
+        st.write("Keine Bestellungen vorhanden.")
+
+if __name__ == '__main__':
+    display_werkzeugnis_results()
