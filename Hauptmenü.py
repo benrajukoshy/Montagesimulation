fe8fdb3ab844e4b364bc471d9d036bef5d74c4d1
Würschinger <vi50jepi@fauad.fau.de>
2023-10-21 23:06:35 +0200

    Update Hauptmenü.py

--- Hauptmenü.py
+++ Hauptmenü.py
@@ -1,5 +1,8 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
+from PIL import Image
+
+image = Image.open('bilder/layout.png')
 st.sidebar.image("https://www.fau.de/files/2021/10/neues-fau-logo-nur-fuer-webmeldung-1024x198.jpg", use_column_width=True)
 
 st.markdown("# Hauptmenü 🏚️")
@@ -9,4 +12,4 @@ st.title("Montagesimulation")
 st.write("Im Rahmen eines Praktikums wird die Analyse und Optimierung nach Methoden der schlanken Produktion vermittelt.")
 st.write("Diese Streamlit App dient der digitalen Abbildung von Bestellungen, deren Bearbeitung und Qualitätskontrolle")
 
-st.image("bilder/layout.png", caption='Layout mit den Arbeitsstationen')
+st.image(image, caption='Layout mit den Arbeitsstationen')
