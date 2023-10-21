46ccec4812276ce77ec48f580879ffdec15a91d9
Würschinger <vi50jepi@fauad.fau.de>
2023-10-21 22:53:47 +0200

    Update Hauptmenü.py

--- Hauptmenü.py
+++ Hauptmenü.py
@@ -1,8 +1,11 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
+st.sidebar.image("https://www.fau.de/files/2021/10/neues-fau-logo-nur-fuer-webmeldung-1024x198.jpg", use_column_width=True)
 
 st.markdown("# Hauptmenü 🏚️")
 st.sidebar.markdown("# Hauptmenü 🏚️")
 
+st.title("Montagesimulation für Praktikum")
+st.write("Im Rahmen eines Praktikum zur Analyse und Optimierung nach Methoden der schlanken Produktion können mittels dieser Streamlit App Bestellungen und deren Bearbeitung simuliert werden")
 
 
