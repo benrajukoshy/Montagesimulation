ae8b4628f81052327ba9b3647b11230d3b88a4a2
Würschinger <vi50jepi@fauad.fau.de>
2023-10-21 22:56:07 +0200

    Update Hauptmenü.py

--- Hauptmenü.py
+++ Hauptmenü.py
@@ -5,7 +5,8 @@ st.sidebar.image("https://www.fau.de/files/2021/10/neues-fau-logo-nur-fuer-webme
 st.markdown("# Hauptmenü 🏚️")
 st.sidebar.markdown("# Hauptmenü 🏚️")
 
-st.title("Montagesimulation für Praktikum")
-st.write("Im Rahmen eines Praktikum zur Analyse und Optimierung nach Methoden der schlanken Produktion können mittels dieser Streamlit App Bestellungen und deren Bearbeitung simuliert werden")
+st.title("Montagesimulation")
+st.write("Im Rahmen eines Praktikums wird die Analyse und Optimierung nach Methoden der schlanken Produktion vermittelt.")
+st.write("Mittels dieser Streamlit App können Bestellungen, deren Bearbeitung und Qualitätskontrolle digital abgebildet werden")
 
 
