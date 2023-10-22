15b0385bfc20ccc6064ff3df45979766472a7c0d
Würschinger <vi50jepi@fauad.fau.de>
2023-10-22 14:59:29 +0200

    Update Bestelleingang.py

--- pages/Bestelleingang.py
+++ pages/Bestelleingang.py
@@ -39,15 +39,15 @@ def display_results():
             time.sleep(1)
         
         #play sound after time
-            sample_rate = 44100  # 44100 samples per second
-            seconds = 2  # Note duration of 2 seconds
-            frequency_la = 440  # Our played note will be 440 Hz
-            # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-            t = np.linspace(0, seconds, seconds * sample_rate, False)
-            # Generate a 440 Hz sine wave
-            note_la = np.sin(frequency_la * t * 2 * np.pi)
-            st.audio(note_la, sample_rate=sample_rate)
-            time.sleep(4)
+        sample_rate = 44100  # 44100 samples per second
+        seconds = 2  # Note duration of 2 seconds
+        frequency_la = 440  # Our played note will be 440 Hz
+        # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+        t = np.linspace(0, seconds, seconds * sample_rate, False)
+        # Generate a 440 Hz sine wave
+        note_la = np.sin(frequency_la * t * 2 * np.pi)
+        st.audio(note_la, sample_rate=sample_rate)
+        time.sleep(4)
         # Timer abgeschlossen
         timer_placeholder.empty()
         st.write("Countdown-Timer abgelaufen.")
