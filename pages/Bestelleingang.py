42e34c773c5a21e6bb708d5dc6607f75c10c9f33
Würschinger <vi50jepi@fauad.fau.de>
2023-10-23 15:28:40 +0200

    Update Bestelleingang.py

--- pages/Bestelleingang.py
+++ pages/Bestelleingang.py
@@ -51,7 +51,7 @@ def display_results():
         st.write("Wenn für bestimmte Bestandteile keine Farbangaben gemacht wurden, dann können diese frei gewählt werden")
         # Countdown-Timer für Kundentakt
         timer_placeholder = st.empty()
-        for i in range(df["Kundentakt"], -1, -1):
+        for i in range(df["Kundentakt"].values[0], -1, -1):
             timer_text = f"<strong><span style='font-size: 2em;'>Zeit bis zum Ausliefern (Kundentakt): {i} Sekunden</span></strong>"
             timer_placeholder.markdown(timer_text, unsafe_allow_html=True)
             time.sleep(1)
