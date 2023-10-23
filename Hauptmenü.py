be91ef51a69d911f52ba93a854460a2d10bc9df2
Würschinger <vi50jepi@fauad.fau.de>
2023-10-23 10:48:46 +0200

    anpassung

--- Hauptmenü.py
+++ Hauptmenü.py
@@ -17,7 +17,7 @@ col1, col2 = st.columns(2)
 
 with col1:
    st.header("Montagelayout")
-   st.image("qr_Code.JPG",width=300)
+   st.image("layout.JPG",width=300)
 
 with col2:
    st.header("Link zum Tool")
