e9a85c43873907cc5c02e174fdeed5b90a7185bf
Würschinger <vi50jepi@fauad.fau.de>
2023-10-23 08:59:47 +0200

    Update Werkszeugnisübersicht.py

--- pages/Werkszeugnisübersicht.py
+++ pages/Werkszeugnisübersicht.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import json
 import pandas as pd
+
+# Setze Streamlit-Option für die Spaltenbreite
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
 # Contents of ~/my_app/main_page.py
 import streamlit as st
 
@@ -22,9 +27,24 @@ def display_werkzeugnis_results():
 
     # Wenn Daten vorhanden sind, diese in einer Tabelle anzeigen
     if werkzeugnis_data:
-        st.write("Werkzeugnisdaten:")
-        df = pd.DataFrame(werkzeugnis_data)
-        st.dataframe(df)
+        # Erstellen eines leeren DataFrames mit den erforderlichen Spalten
+        
+        df = pd.DataFrame(columns=["Bestelldatum und Uhrzeit:", "Kunde:", "Sonderwunsch:", "Führerhaus:", "Sidepipes:", "Container 1:", "Container 2:", "Container 3:", "Container 4:"])
+        
+        for idx, entry in enumerate(werkzeugnis_data, start=1):
+            df.loc[idx] = [
+                entry["Bestelldatum und Uhrzeit"],
+                entry["Kunde"],
+                entry["Sonderwunsch"],
+                entry["Variante nach Bestellung"].get("Führerhaus", "N/A"),
+                entry["Variante nach Bestellung"].get("Sidepipes", "N/A"),
+                entry["Variante nach Bestellung"].get("Container 1", "N/A"),
+                entry["Variante nach Bestellung"].get("Container 2", "N/A"),
+                entry["Variante nach Bestellung"].get("Container 3", "N/A"),
+                entry["Variante nach Bestellung"].get("Container 4", "N/A")
+            ]
+        
+        st.dataframe(df.T, use_container_width= True)  # Transponieren des DataFrames und Anzeigen als Tabelle
         
     else:
         st.write("Keine Werkzeugnisdaten vorhanden.")
