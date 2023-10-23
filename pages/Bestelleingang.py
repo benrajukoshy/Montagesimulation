fc0ef3d7ec8b420045a7a8829dc83a30149f3059
Würschinger <vi50jepi@fauad.fau.de>
2023-10-23 15:32:58 +0200

    Update Bestelleingang.py

--- pages/Bestelleingang.py
+++ pages/Bestelleingang.py
@@ -42,7 +42,7 @@ def display_results():
             ]
         
         st.dataframe(df.T, use_container_width= True)  # Transponieren des DataFrames und Anzeigen als Tabelle
-        st.write("Dies ist der Kundentakt:", df["Kundentakt"].values[0])
+        
 
         #bestellungen_database_filename = "bestellungen_database.json"
         #bestellungen_data = display_results(bestellungen_database_filename)
