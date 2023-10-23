073d14d658010ee6b1940e2a5bc520bf6ed951b4
Würschinger <vi50jepi@fauad.fau.de>
2023-10-23 09:12:10 +0200

    Update Werkszeugnisübersicht.py

--- pages/Werkszeugnisübersicht.py
+++ pages/Werkszeugnisübersicht.py
@@ -6,18 +6,10 @@ import pandas as pd
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-# Contents of ~/my_app/main_page.py
-import streamlit as st
-
-st.markdown("# Werkzeugnisübersicht ⭐")
-st.sidebar.markdown("# Werkzeugnisübersicht ⭐")
-
 # Dateiname der Datenbank
 database_filename = "werkzeugnis_database.json"
 
 def display_werkzeugnis_results():
-    
-
     # Laden der Werkzeugnisdaten aus der JSON-Datei
     werkzeugnis_data = []
     with open(database_filename, "r") as db:
@@ -28,19 +20,21 @@ def display_werkzeugnis_results():
     # Wenn Daten vorhanden sind, diese in einer Tabelle anzeigen
     if werkzeugnis_data:
         # Erstellen eines leeren DataFrames mit den erforderlichen Spalten
-        
-        df = pd.DataFrame(columns=["Bestelldatum:", "Kunde:", "Sonderwunsch:", "Führerhaus:", "Sidepipes:", "Container 1:", "Container 2:", "Container 3:", "Container 4:"])
-        
-        for idx, entry in enumerate(werkzeugnis_data, start=1):
+        df = pd.DataFrame(columns=["Bestelldatum:", "Kunde:"], index=[1])  # Nur 2 Spalten
+        idx = 1  # Startindex
+
+        for entry in werkzeugnis_data:
             df.loc[idx] = [
                 entry["Bestelldatum"],
-                entry["Kunde"]]                
-                #entry["Variante nach Bestellung"].get("Qualitätsprüfung", "N/A")]
-        
-        st.dataframe(df.T, use_container_width= True)  # Transponieren des DataFrames und Anzeigen als Tabelle
-        
+                entry["Kunde"]
+            ]
+            idx += 1  # Inkrementiere den Index für jede Zeile
+
+        st.dataframe(df.T, use_container_width=True)  # Transponieren des DataFrames und Anzeigen als Tabelle
+
     else:
         st.write("Keine Werkzeugnisdaten vorhanden.")
 
 if __name__ == '__main__':
     display_werkzeugnis_results()
+
