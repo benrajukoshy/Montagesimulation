ae3514823cadc713a571f702cc50abb616ae1c8c
Würschinger <vi50jepi@fauad.fau.de>
2023-10-24 21:21:25 +0200

    Update BearbeiteteAufträge.py

--- pages/BearbeiteteAufträge.py
+++ pages/BearbeiteteAufträge.py
@@ -41,7 +41,7 @@ if data is not None:
         x='Kunde:N',
         y='Zeitdifferenz:Q',
         color=alt.condition(
-            alt.datum.results(Kundentakt < Zeitdifferenz),
+            alt.datum.["Kundentakt"] > alt.datum["Zeitdifferenz"],
             alt.value('rot'), alt.value('green')
         )
     )
