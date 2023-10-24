d6d029658e5f17d337d8ea411e3933d15bdca1f8
Würschinger <vi50jepi@fauad.fau.de>
2023-10-24 19:42:56 +0200

    Create BearbeiteteAufträge.py

---
+++ pages/BearbeiteteAufträge.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+# Lade die CSV-Datei
+@st.cache
+def load_data():
+    data = pd.read_csv("bearbeitsungsstatus.csv")
+    return data
+
+data = load_data()
+
+# Zeige die Daten als Tabelle
+st.write("Bearbeitungsstatus CSV-Datei")
+st.write(data)
+
+# Füge einen Download-Button hinzu
+st.download_button(
+    label="CSV-Datei herunterladen",
+    data=data.to_csv(index=False).encode('utf-8'),
+    file_name="bearbeitsungsstatus.csv",
+    key="download-button"
+)
