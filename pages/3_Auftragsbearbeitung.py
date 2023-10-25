b9697b24109205b97243485aef6521f8958b866c
Würschinger <vi50jepi@fauad.fau.de>
2023-10-25 22:20:40 +0200

    Update 3_Auftragsbearbeitung.py

--- pages/3_Auftragsbearbeitung.py
+++ pages/3_Auftragsbearbeitung.py
@@ -61,7 +61,7 @@ def save_to_csv(data):
     with open(filename, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         # Schreibe die Kopfzeile
-        #csv_writer.writerow(header)
+        csv_writer.writerow(header)
         csv_writer.writerows(rows)
 # ...
 
