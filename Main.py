f416335b55e4f0da28af41319e80dcafa315c96f
benrajukoshy <152288350+benrajukoshy@users.noreply.github.com>
2023-12-05 13:55:56 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    # Set up the main page
+    st.title("Meine Streamlit App")
+    st.header("Willkommen auf der Hauptseite")
+    
+    # Display some test text
+    st.write("Das ist ein Testtext auf der Hauptseite der Streamlit-App.")
+
+if __name__ == "__main__":
+    main()
